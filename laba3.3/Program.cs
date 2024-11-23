a3c85b7da6e56e0b11be2869e7a22e7f06ae0c7c
dirose <dirose@DESKTOP-PQA70RD>
2024-11-23 16:40:20 +0200

    Добавьте файлы проекта.

---
+++ laba3.3/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите значение x:");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите количество членов ряда:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double result = CalculateTaylorSeries(x, n);
+
+Console.WriteLine($"Результат ряда Тейлора для sin(x) при x = {x} и {n} членах: {result}");
+    
+
+   double CalculateTaylorSeries(double x, int n)
+{
+    double sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        double term = Math.Pow(-1, i) * Math.Pow(x, 2 * i + 1) / Factorial(2 * i + 1);
+        sum += term;
+    }
+
+    return sum;
+}
+
+ double Factorial(int number)
+{
+    if (number == 0 || number == 1)
+        return 1;
+
+    double factorial = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
